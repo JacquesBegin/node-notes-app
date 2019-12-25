fd27af27942189988961a4786918e1687478e0d2
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-24 23:03:13 -0500

    Creates a yargs command to add a new note.

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const getNotes = require("./notes.js");
 // Customize yargs version
 yargs.version("1.1.0");
 
-
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note");
+  }
+});
 
 
 console.log(yargs.argv);
