7def93a37e30fe9e31442c7fc35b7b40bcaa941e
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-24 22:58:44 -0500

    Adds comment about proper order for loading requirements.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// A good practice is to list core modules then third party followed by required files.
 const chalk = require("chalk");
 const yargs = require("yargs");
-
 const getNotes = require("./notes.js");
+
+
 
 console.log(yargs.argv);
