33438d76d99b6426cf08ac0545ed60cfbd9f92d2
Jacques Begin <jacques.andre.begin@gmail.com>
2019-12-24 22:59:39 -0500

    Customizes yargs version.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const chalk = require("chalk");
 const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
+// Customize yargs version
+yargs.version("1.1.0");
+
+
 
 
 console.log(yargs.argv);
